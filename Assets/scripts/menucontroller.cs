c168d956cf630b09ee8ecebef529913c3007719a
milzux <milla.jaaskelainen@gmail.com>
2022-12-08 20:40:32 +0200

    menuscene valmis

--- Assets/scripts/menucontroller.cs
+++ Assets/scripts/menucontroller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.Networking;
 
 public class menucontroller : MonoBehaviour
 {
@@ -11,12 +12,15 @@ public class menucontroller : MonoBehaviour
     public TMP_InputField nimi;
     public Button exitbtn;
     public string PlayerName;
+    public TMP_Text scoreboard;
+    string url = "172.30.139.31/unity/manage_request.php";
 
     // Start is called before the first frame update
     void Start()
     {
         startgame.onClick.AddListener(LevelOneLoader);
         exitbtn.onClick.AddListener(Yeet);
+        StartCoroutine(SendGR());
     }
 
     // Update is called once per frame
@@ -27,14 +31,45 @@ public class menucontroller : MonoBehaviour
 
     void LevelOneLoader()
     {
-        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
-        //Pelaajan nimi PlayerName-muuttujassa!!
         string name = nimi.text.ToString();
-        PlayerPrefs.SetString(PlayerName, name);
+        if (name != "Kirjoita nimi" && name != "") {
+            SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+            //Pelaajan nimi PlayerName-muuttujassa!!
+            PlayerPrefs.SetString(PlayerName, name);
+        }
+        else
+        {
+            StartCoroutine(NimiPuuttuu());
+        }
+    }
+
+    IEnumerator NimiPuuttuu()
+    {
+        nimi.image.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        nimi.image.color = Color.white;
     }
 
     void Yeet()
     {
         Application.Quit();
+    }
+
+    IEnumerator SendGR()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(url + "/?unityget="))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                scoreboard.text = responseText;
+            }
+        }
     }
 }
